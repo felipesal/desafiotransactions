de298be2729c2ad0bd61841adccbcc4176152ae0
Felipe Salles <prof.felipe.salles@gmail.com>
2020-06-23 01:44:21 -0300

    First commit - project created

---
+++ src/main/java/com/picpay/transactions/DesafiotransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiotransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay/transactions/DesafiotransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
